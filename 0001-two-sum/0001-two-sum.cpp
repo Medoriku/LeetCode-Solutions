46dc9987c505ab90c48becbf20ef04e453c06268
Medoriku <80148071+Medoriku@users.noreply.github.com>
2024-07-28 16:57:13 -0600

    Time: 48 ms (39.18%), Space: 12.9 MB (59.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Loop through the elements of the vector
         for (int i = 0; i != nums.size(); ++i){
+            // Loop through the elements of the vector excluding the i and elements before i
             for (int j = i + 1; j != nums.size(); ++j){
+                // If the sum of the element of the vector with the index i and element of the vector with the index j equal the target integer, return the indices of those elements
                 if (nums[i] + nums[j] == target){
                     return {i, j};
                 } 
