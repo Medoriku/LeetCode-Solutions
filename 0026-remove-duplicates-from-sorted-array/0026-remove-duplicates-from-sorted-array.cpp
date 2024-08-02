65cfb1265d90ff001cbe8366d98517e89a5dfeaa
Medoriku <80148071+Medoriku@users.noreply.github.com>
2024-08-01 21:31:05 -0600

    Time: 14 ms (13.93%), Space: 22.7 MB (6.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        std:: unordered_set<int> seen;
+        std::vector<int> result;
+        int count = 0;
+
+        // for each element in nums vector
+        for (int num:nums){
+            // If num is not found in the seen set
+            if (seen.find(num) == seen.end()){
+                // Add the current element to seen set
+                seen.insert(num);
+                // Increase the count of unique elements
+                nums[count++] = num;
+            }
+        }
+        return count;
+    }
+};
