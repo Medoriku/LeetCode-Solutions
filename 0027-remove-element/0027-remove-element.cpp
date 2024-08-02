9bf63903b7b46379b6720e51e1106be9211ea947
Medoriku <80148071+Medoriku@users.noreply.github.com>
2024-08-02 00:08:38 -0600

    Time: 5 ms (27.56%), Space: 10.7 MB (27.94%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int count = 0;
-        
+        // for every element in nums vector
         for(int i = 0; i < nums.size(); ++i){
+            // if the element does not equal val
             if(nums[i] != val){
+                // place the element at the current position of count
                 nums[count++] = nums[i];
             }
         }
