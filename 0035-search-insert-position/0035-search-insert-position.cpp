e6969c98c33ce0e1d7d3b701056ad10b0eb68b1e
Medoriku <80148071+Medoriku@users.noreply.github.com>
2024-08-06 16:55:35 -0600

    Time: 3 ms (71.5%), Space: 12.4 MB (27.72%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int size = nums.size();
+        // Case when target is less than the first element
+        if (target < nums[0]){
+            return 0;
+        }
+
+        //Case when target is larger than the last element
+        if (target > nums[size - 1]){
+            return size;
+        }
+
+        //Case when target is in the range from the first to last element
+        for (int i = 0; i < (size); i++){
+            if (target == nums[i]){
+                return i;
+            }
+            if (target > nums[i] && target < nums[i+1]){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
