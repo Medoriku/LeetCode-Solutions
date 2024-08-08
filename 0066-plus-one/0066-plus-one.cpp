efb1ae3e510d83e2a9c71d95d73a48f35f2c473a
Diyora Daminova <80148071+Medoriku@users.noreply.github.com>
2024-08-07 23:07:42 -0600

    Time: 6 ms (26.57%), Space: 10.6 MB (22.2%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
+        // Loop through integers from the end of the vector
         for (int i = n-1; i >= 0; --i) {
+            // Check if the digits are less than 9 and if so add 1
             if (digits[i] < 9){
                 digits[i] += 1;
                 return digits;
             }
+
+            // If the digit is 9 set it to 0
             digits[i] = 0;
         } 
+
+        // Insert 1 at the beginning of the vector if the last digit is 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
