6983735c486dee5da55fad39a50bbed57f2fdd70
Diyora Daminova <80148071+Medoriku@users.noreply.github.com>
2024-08-28 13:15:43 -0600

    Time: 12 ms (29.96%), Space: 10 MB (13.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int counter = 0;
-
-        if (s.length() != t.length()){
+        // If the strings are of different lengths, they can't be anagrams
+        if (s.length() != t.length()) {
             return false;
         }
-
-        for (int i = 0; i < s.length(); ++i){
-            if (t.find(s[i]) != string::npos){
-                counter += 1;
-                t.erase(t.find(s[i]), 1);
-                s.erase(i, 1);
-                continue;
-            }
-            else if (t.find(s[i]) == string::npos){
-                return false;
-            }
+        // Use a hash map to count the frequency of each character in 's'
+        unordered_map<char, int> count;
+        for (char c : s) {
+            count[c]++;
         }
-        cout << counter;
-        return (s.length() == t.length());
 
+        // Decrease the frequency based on characters in 't'
+        for (char c : t) {
+            if (count[c] == 0) {
+                return false; // More occurrences of this character in 't' than in 's'
+            }
+            count[c]--;
+        }
+
+        // If the map is balanced, the strings are anagrams
+        return true;
     }
 };
