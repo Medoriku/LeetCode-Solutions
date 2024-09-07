078cb5ba1a94dfe985dae208bb36874f1db3d2dd
Diyora Daminova <80148071+Medoriku@users.noreply.github.com>
2024-09-06 18:39:44 -0600

    Time: 5 ms (30.29%), Space: 10.4 MB (85.36%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        
-        // Проверяем каждое возможное значение x от 0 до n
         for (int x = 0; x <= n; ++x) {
             int count = 0;
             for (int num : nums) {
@@ -11,12 +9,11 @@ public:
                     count++;
                 }
             }
-            // Если количество чисел равно x, возвращаем x
             if (count == x) {
                 return x;
             }
         }
-        
-        return -1;  // Если не нашли такого x, возвращаем -1
+
+        return -1;
     }
 };
