3ac5041bd0a9b3698b7c42cb059c789c7c88cc8d
Diyora Daminova <80148071+Medoriku@users.noreply.github.com>
2024-09-09 13:06:19 -0600

    Time: 2174 ms (5.31%), Space: 27 MB (49%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
         for (int i = 0; i < nums.size(); ++i) {
             int rep_count = count(nums.begin(), nums.end(), nums[i]);
             if (rep_count > (nums.size() / 2)) {
