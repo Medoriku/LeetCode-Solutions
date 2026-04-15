5667088de0517618468ff7242fb7b815c93a2c9e
Diyora Daminova <80148071+Medoriku@users.noreply.github.com>
2026-04-15 12:14:34 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Loop through the elements of the vector
-        for (int i = 0; i != nums.size(); ++i){
-            // Loop through the elements of the vector excluding the i and elements before i
-            for (int j = i + 1; j != nums.size(); ++j){
-                // If the sum of the element of the vector with the index i and element of the vector with the index j equal the target integer, return the indices of those elements
-                if (nums[i] + nums[j] == target){
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j && nums[i] + nums[j] == target) {
                     return {i, j};
-                } 
+                }
             }
         }
+
         return {};
     }
 };
